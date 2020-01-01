c9bde578ee8ee7da78f526c9c89965b3f72070a2
Holly Davis <hollydavis541@gmail.com>
2020-01-01 01:05:48 -0800

    created filterByTerm.js file

---
+++ src/filterByTerm.js
@@ -0,0 +1,8 @@
+function filterByTerm(inputArr, searchTerm) {
+  if (!searchTerm) throw Error("searchTerm cannot be empty");
+  const regex = new RegExp(searchTerm, "i");
+  return inputArr.filter(function(arrayElement) {
+    return arrayElement.url.match(regex);
+  });
+}
+module.exports = filterByTerm;
