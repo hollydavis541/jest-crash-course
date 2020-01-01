17ef6db4ced2a78c10b8973692d23d71be63d816
Holly Davis <hollydavis541@gmail.com>
2020-01-01 00:57:06 -0800

    added empty search test

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,28 +1,30 @@
 function filterByTerm(inputArr, searchTerm) {
-  const regex = new RegExp(searchTerm, "i");
+  const regex = new RegExp(searchTerm, 'i');
   return inputArr.filter(function(arrayElement) {
     return arrayElement.url.match(regex);
   });
 }
 
-describe("Filter function", () => {
-  test("it should filter by a search term (link)", () => {
-    const input = [
-      { id: 1, url: "https://www.url1.dev" },
-      { id: 2, url: "https://www.url2.dev" },
-      { id: 3, url: "https://www.link3.dev" }
-    ];
-    const output = [{ id: 3, url: "https://www.link3.dev" }];
-    expect(filterByTerm(input, "link")).toEqual(output);
-    expect(filterByTerm(input, "LINK")).toEqual(output);
+describe('Filter function', () => {
+  const input = [
+    { id: 1, url: 'https://www.url1.dev' },
+    { id: 2, url: 'https://www.url2.dev' },
+    { id: 3, url: 'https://www.link3.dev' }
+  ];
+  test('it should filter by a search term (link)', () => {
+    
+    const output = [{ id: 3, url: 'https://www.link3.dev' }];
+    expect(filterByTerm(input, 'link')).toEqual(output);
+    expect(filterByTerm(input, 'LINK')).toEqual(output);
   });
-  test("it should filter by a search term (url)", () => {
-    const input = [
-      { id: 1, url: "https://www.url1.dev" },
-      { id: 2, url: "https://www.url2.dev" },
-      { id: 3, url: "https://www.link3.dev" }
-    ];
-    const output = [{ id: 1, url: "https://www.url1.dev" }, { id: 2, url: "https://www.url2.dev" }];
-    expect(filterByTerm(input, "uRl")).toEqual(output);
+  test('it should filter by a search term (url)', () => {
+
+    const output = [{ id: 1, url: 'https://www.url1.dev' }, { id: 2, url: 'https://www.url2.dev' }];
+    expect(filterByTerm(input, 'uRl')).toEqual(output);
+  });
+  test('empty search term returns all urls', () => {
+
+    const output = [{ id: 1, url: 'https://www.url1.dev' }, { id: 2, url: 'https://www.url2.dev' }, { id: 3, url: 'https://www.link3.dev' }];
+    expect(filterByTerm(input, '')).toEqual(output);
   });
 });
