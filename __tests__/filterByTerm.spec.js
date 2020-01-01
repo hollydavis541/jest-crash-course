18d024b86d0ebaede503b9336b6a5e8493104f6d
Holly Davis <hollydavis541@gmail.com>
2020-01-01 00:42:41 -0800

    added url search term test

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -16,4 +16,13 @@ describe("Filter function", () => {
     expect(filterByTerm(input, "link")).toEqual(output);
     expect(filterByTerm(input, "LINK")).toEqual(output);
   });
+  test("it should filter by a search term (url)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+    const output = [{ id: 1, url: "https://www.url1.dev" }, { id: 2, url: "https://www.url2.dev" }];
+    expect(filterByTerm(input, "uRl")).toEqual(output);
+  });
 });
